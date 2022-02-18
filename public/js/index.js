f6b3abc9dfae1588cd77ca7b2675a81293d93d3c
franco.balich@gmail.com <franco.balich@gmail.com>
2022-02-18 19:39:25 -0300

    Finish menu responsive

--- public/js/index.js
+++ public/js/index.js
@@ -1,32 +1,21 @@
-const   pagina = document.querySelector("body"),
-        btnMenu = document.querySelector(".btnMenu"),
-        menu = document.querySelector(".menu");
+const   btnMenu = document.getElementById("btnMenu"),
+        btnMenuCerrar = document.getElementById("btnMenuCerrar"),
+        menu = document.getElementById("menu");
 
-let state = false;
-let msg=btnMenu.innerHTML;
-
-btnMenu.innerHTML = btnMenu.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+let state = true;
 
 btnMenu.addEventListener("click", () => {
-    if (!state) {
-        menu.style.top = "0vh";
-        
-        btnMenu.style.opacity="0";
-        setTimeout(function() {
-            btnMenu.innerHTML="Cerrar";
-            
-        btnMenu.style.opacity="1";
-            state = true;
-          }, 500);
+    if (state) {
+        menu.style.left = "0px";
+        btnMenu.style.top="-70px";
+        state=false;
     }
-    else{;
-        menu.style.top = "-100vh";
-        btnMenu.style.opacity="0";
-        setTimeout(function() {
-            btnMenu.innerHTML=msg;
-            btnMenu.style.opacity="1";
-            state = false;
-          }, 500);
-        
+});
+
+btnMenuCerrar.addEventListener("click", () => {
+    if (!state) {
+        menu.style.left = "-100%";
+        btnMenu.style.top="0px";
+        state=true;
     }
 });
