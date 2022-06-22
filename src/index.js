2805c57b63df43b7bd23dab7888e5f1a9e72b061
franco.balich@gmail.com <franco.balich@gmail.com>
2022-06-22 00:55:10 -0300

    Add start js

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+const port = process.env.PORT || 5500;
+const app = express();
+const server = require("http").createServer(app);
+
+const iniciarServer = () => {
+  var publicPath = path.resolve(__dirname, "../views");
+  app.use(express.static(publicPath));
+  // set the view engine to ejs
+  app.set("view engine", "ejs");
+
+  app.get("/", function (req, res) {
+    res.render("pages/index.ejs");
+  });
+  app.get("/links", function (req, res) {
+    res.render("pages/links.ejs");
+  });
+  app.get("/contacto", function (req, res) {
+    res.render("pages/contacto.ejs");
+  });
+  server.listen(port, () => {
+    console.log(`El server esta corriendo el puerto:${port}`);
+  });
+};
+iniciarServer();
